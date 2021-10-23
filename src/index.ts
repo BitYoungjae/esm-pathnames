b37eff9e90b97476022b759055efdc132fb0b4d3
BitYoungjae <bityoungjae@gmail.com>
2021-10-23 15:59:09 +0900

    Update deps and change task of build

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
-interface importMeta {
-  url: string;
-}
-
 /**
  * Extract __dirname and __filename from import.meta
  *
  * @param meta import.meta ex: getPathNames(import.meta)
  */
 const getPathNames = (
-  meta: importMeta,
+  meta: ImportMeta,
 ): { __filename: string; __dirname: string } => {
   const __filename = fileURLToPath(meta.url);
   const __dirname = dirname(__filename);
