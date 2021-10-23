64f2880e329e467949107e1bd3603a17aacf250a
BitYoungjae <bityoungjae@gmail.com>
2021-10-23 20:19:57 +0900

    Add intended wrong code

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { dirname } from 'path';
  */
 const getPathNames = (
   meta: ImportMeta,
-): { __filename: string; __dirname: string } => {
+): { __filename: number; __dirname: string } => {
   const __filename = fileURLToPath(meta.url);
   const __dirname = dirname(__filename);
 
